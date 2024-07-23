db8955a373bddc32452887f544d8f99d1c255584
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-07-23 16:26:53 +0300

    11

--- Angular/rozetka-clone/src/main.ts
+++ Angular/rozetka-clone/src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser'
-import { appConfig } from './app/app.config'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 import { AppComponent } from './app/app.component'
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, {
+    providers: [provideHttpClient(withFetch())],
+}).catch((err) => console.error(err))
